514bfb99896342610a758ebc11e541a2f7983126
3daniel1996 <dandsmith96@gmail.com>
2023-10-24 16:01:56 -0700

    add JMS BROKER Secrets/Env. Variable

--- Infrastructure/secretmanager.tf
+++ Infrastructure/secretmanager.tf
@@ -61,6 +61,14 @@ resource "aws_secretsmanager_secret" "api_url" {
   name = "API_URL_PRESIGNED_S3"
 }
 
+resource "aws_secretsmanager_secret" "jms_user" {
+  name = "JMS_BROKER_USERNAME"
+}
+
+resource "aws_secretsmanager_secret" "jms_pass" {
+  name = "JMS_BROKER_PASSWORD"
+}
+
 resource "aws_secretsmanager_secret_version" "rds_credentials" {
   secret_id     = aws_secretsmanager_secret.pg_user.id
   secret_string = <<EOF
@@ -140,5 +148,15 @@ resource "aws_secretsmanager_secret_version" "api_key" {
 }
 resource "aws_secretsmanager_secret_version" "api_url" {
   secret_id     = aws_secretsmanager_secret.api_url.id
+  secret_string = "changeme"
+}
+
+resource "aws_secretsmanager_secret_version" "jms_user" {
+  secret_id     = aws_secretsmanager_secret.jms_user.id
+  secret_string = "changeme"
+}
+
+resource "aws_secretsmanager_secret_version" "jms_pass" {
+  secret_id     = aws_secretsmanager_secret.jms_pass.id
   secret_string = "changeme"
 }

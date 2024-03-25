8f9532ddd7e0e2770c3537f1b907c7366b0067fc
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2024-03-25 14:02:17 -0700

    Update rds.tf

--- Infrastructure/rds.tf
+++ Infrastructure/rds.tf
@@ -42,7 +42,7 @@ module "postgres_rds" {
   major_engine_version = "13"
   family               = "postgres13"
   engine               = "postgres"
-  engine_version       = "13.10"
+  engine_version       = "13.13"
   instance_class       = "db.t3.micro"
   allocated_storage    = 5
 
